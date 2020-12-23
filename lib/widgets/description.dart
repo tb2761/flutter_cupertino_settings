d3a836ecedb094f00e6c29190b6ecc1ba5793713
bierbaumtim <tb2706@outlook.de>
2020-12-23 13:20:20 +0100

    Expose Padding parameter on CSDescription

--- lib/widgets/description.dart
+++ lib/widgets/description.dart
@@ -4,17 +4,19 @@ class CSDescription extends StatelessWidget {
   final String description;
   final Color backgroundColor;
   final TextStyle style;
+  final EdgeInsets padding;
 
   const CSDescription(
     this.description, {
     this.backgroundColor,
     this.style,
+    this.padding,
   }) : assert(description != null);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.fromLTRB(10, 7.5, 5, 5),
+      padding: padding ?? const EdgeInsets.fromLTRB(10, 7.5, 5, 5),
       color: backgroundColor ??
           CupertinoColors.systemGroupedBackground.resolveFrom(context),
       child: Text(
